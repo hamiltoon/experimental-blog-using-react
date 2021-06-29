5b8f3000259c9584a08b7bdc2aa9b197c7d5b877
John Alm Mårtensson <joal@bonnierforalgen.se>
2021-06-29 13:56:28 +0200

    Adding generic type to validation

--- src/form/validation.tsx
+++ src/form/validation.tsx
@@ -4,53 +4,56 @@ type Value = string | number | undefined
 
 type Rule = (value?: Value) => String | undefined
 
-type MessageFunction = (numOfChars: any) => string
+type ErrorMessageFunction = (numOfChars: any) => string
 
-type Message = String
+type ErrorMessage = String
+
+type MaybeValid = ErrorMessage | undefined
 
 const valueIsEmpty: (value: any) => boolean = (value: Value) =>
   isNil(value) || (isString(value) && value.trim() === '')
 
-export const notEmpty = (message?: string): Rule => (value: Value) => {
-  return valueIsEmpty(value) ? (!!message ? message : 'Need to be entered.') : undefined
-}
+export const notEmpty =
+  (message?: ErrorMessage): Rule =>
+  (value: Value) => {
+    return valueIsEmpty(value) ? (!!message ? message : 'Need to be entered.') : undefined
+  }
 
-export const hasMaxLength = (message?: Message | MessageFunction) => (maxLength: number): Rule => (
-  value: Value,
-) => {
-  const actualMessage = isFunction(message) ? message(maxLength) : message
-  const hasError = !valueIsEmpty(value) && isString(value) && value.length >= maxLength
+export const hasMaxLength =
+  (message?: ErrorMessage | ErrorMessageFunction) =>
+  (maxLength: number): Rule =>
+  (value: Value) => {
+    const actualMessage = isFunction(message) ? message(maxLength) : message
+    const hasError = !valueIsEmpty(value) && isString(value) && value.length >= maxLength
 
-  return hasError
-    ? actualMessage
+    return hasError
       ? actualMessage
-      : `Need to be less than ${maxLength} characters`
-    : undefined
-}
+        ? actualMessage
+        : `Need to be less than ${maxLength} characters`
+      : undefined
+  }
 
-export const isEmailAddress = (message?: string): Rule => (value: Value) => {
-  const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  const hasError = !valueIsEmpty(value) && isString(value) && !EMAIL_REGEX.test(value)
+export const isEmailAddress =
+  (message?: ErrorMessage): Rule =>
+  (value: Value) => {
+    const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+    const hasError = !valueIsEmpty(value) && isString(value) && !EMAIL_REGEX.test(value)
 
-  return hasError ? (message ? message : 'The email address is not valid') : undefined
-}
+    return hasError ? (message ? message : 'The email address is not valid') : undefined
+  }
 
 const runValidators = (currentRules: Rule[], value: Value) => {
   const allErrors = currentRules.map((validator) => validator(value)).filter((_) => _)
   return first(allErrors)
 }
 
-export const validate: (
-  validationRules: Record<string, Rule[]>,
-) => (values: Record<string, Value>) => Record<string, Value> = (validationRules) => (
-  values: Record<string, Value>,
-) => {
-  return Object.entries(validationRules).reduce((acc, currentRuleSet) => {
-    const [ruleKey, rules] = currentRuleSet
-    const currentValue: Value = values[ruleKey]
-    return {
-      ...acc,
-      [ruleKey]: runValidators(rules, currentValue),
-    }
-  }, {})
-}
+export const validate = <O, > (validationRules: Record<keyof O, Rule[]>) => (values: Record<string, Value>): Record<string, MaybeValid> => {
+    return Object.entries<Rule[]>(validationRules).reduce( (acc, currentRuleSet) => {
+      const [ruleKey, rules] = currentRuleSet
+      const currentValue: Value = values[ruleKey]
+      return {
+        ...acc,
+        [ruleKey]: runValidators(rules, currentValue),
+      }
+    }, {})
+  }
